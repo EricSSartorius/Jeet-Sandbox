9691583f7e58e0f03a80164aa96c4f8a7680f644
EricSSartorius <EricSSartorius@gmail.com>
2015-05-16 12:40:38 +0900

    gulp plugins and folders added

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,94 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var typographic = require('typographic');
-var nib = require('nib');
-var rupture = require('rupture');
-var jeet= require('jeet');
+//load plugins
+var gulp = require('gulp'),
+	stylus = require('gulp-stylus'),
+	typographic = require('typographic'),
+	nib = require('nib'),
+	rupture = require('rupture'),
+	jeet= require('jeet');
+
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+
+
+
+
 
 gulp.task('styles', function() {
-	gulp.src('style.styl')
+	return gulp.src('style.styl')
 		.pipe(stylus({
-			use: [typographic(), nib(), rupture(), jeet()]
-		}))
-		.pipe(gulp.dest('./'));
+			use: [typographic(), nib(), rupture(), jeet()]}))
+		.pipe(gulp.dest('./styles'))
+
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    	.pipe(gulp.dest('./styles'))
+    	.pipe(rename({ suffix: '.min' }))
+    	.pipe(minifycss())
+    	.pipe(gulp.dest('./styles'))
+    	.pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task('watch:styles', function(){
-	gulp.watch('**/*.styl', ['styles']);
+
+
+
+
+ 
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+ 
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+ 
+// Clean
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+ 
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+ 
+// Watch
+gulp.task('watch', function() {
+ 
+  // Watch .scss files
+  gulp.watch('**/*.styl', ['styles']);
+ 
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+ 
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+ 
+  // Create LiveReload server
+  livereload.listen();
+ 
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+ 
 });
