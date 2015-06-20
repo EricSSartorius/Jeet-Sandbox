7ba69cca41b9fb11e5c96cab9e02ea74b35a53d6
EricSSartorius <EricSSartorius@gmail.com>
2015-06-21 01:39:30 +0900

    started jquery language selector

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,1 +1,22 @@
-//test
+
+/*$('.english').on('click',function(){
+    if($('#en').css('display')!='none'){
+    $('#2').html($('#jp').html()).show().siblings('div').hide();
+    }else if($('#2').css('display')!='none'){
+        $('#en').show().siblings('div').hide();
+    }
+});*/
+
+
+$("div.action").click (function(){
+    var $this = $(this);
+    var target = $this.data('content');
+    $('div.action').not($this).each(function(){
+       var $other = $(this);
+       var otherTarget = $other.data('content');
+       $(otherTarget).hide();        
+    });
+    
+    $(target).animate({width: "toggle"}, 200);
+    
+});
