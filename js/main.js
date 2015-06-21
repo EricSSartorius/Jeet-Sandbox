65186d827eb19d6bc27e9ef15ae943b1ebc9abe9
EricSSartorius <EricSSartorius@gmail.com>
2015-06-21 21:46:37 +0900

    jquery language change on click finished

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
-
-/*$('.english').on('click',function(){
-    if($('#en').css('display')!='none'){
-    $('#2').html($('#jp').html()).show().siblings('div').hide();
-    }else if($('#2').css('display')!='none'){
-        $('#en').show().siblings('div').hide();
-    }
-});*/
-
-
+// Toggle EN/JP/KR for about section
 $("div.action").click (function(){
     var $this = $(this);
-    var target = $this.data('content');
+    var target = $this.data('language');
     $('div.action').not($this).each(function(){
        var $other = $(this);
-       var otherTarget = $other.data('content');
+       var otherTarget = $other.data('language');
        $(otherTarget).hide();        
     });
     
-    $(target).animate({width: "toggle"}, 200);
-    
+    $(target).show();
 });
