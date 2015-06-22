f7a140ad6daa017c3b4c1de3b72b38c945462e8b
EricSSartorius <EricSSartorius@gmail.com>
2015-06-22 15:32:49 +0900

    spyscroll added but needs fixed, resume section added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$(function() {
+
 // Toggle EN/JP/KR for about section
 $("div.action").click (function(){
     var $this = $(this);
@@ -10,3 +12,74 @@ $("div.action").click (function(){
     
     $(target).show();
 });
+
+//Menu Scrollspy
+// Cache selectors
+var lastId,
+    topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
+
+}); //jQuery is loaded
+
+
+
+//active class on click javascript
+/*var selector, elems, makeActive;
+
+selector = 'nav p';
+
+elems = document.querySelectorAll(selector);
+
+makeActive = function () {
+    for (var i = 0; i < elems.length; i++)
+        elems[i].classList.remove('active');
+    
+    this.classList.add('active');
+};
+
+for (var i = 0; i < elems.length; i++)
+    elems[i].addEventListener('mousedown', makeActive);*/
+
+
